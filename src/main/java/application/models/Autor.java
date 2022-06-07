62493fcb33bdc383ca4e5e048c827c5c0ad639e4
danielsbp <danielbatistaodp3@gmail.com>
2022-06-07 18:15:33 +0000

    P2, Explique cada uma linha de uma classe do pacote model

--- src/main/java/application/models/Autor.java
+++ src/main/java/application/models/Autor.java
@@ -1,21 +1,24 @@
-package application.models;
+package application.models; // Define que este arquivo pertence ao pacote models
 
+// Importação das bibliotecas
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
+@Entity // Define a classe a seguir como uma entidade do banco de dados MySQL
 
-@Table(name="autores")
+@Table(name="autores") // Define o nome da entidade como "autores"
 
+// Criação da classe
 public class Autor {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String nome;
+    @Id // Diz que o próximo campo será uma primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Diz que o próximo campo incrementará a cada registro.
+    private int id; // Cria o "id" como inteiro.
+    private String nome; // Cria outro campo chamado "nome", do tipo varchar() ou String;
     
+    // Getters e setters dos atributos "id" e "nome" (Recuperar e setar).
     public int getId() {
         return id;
     }
